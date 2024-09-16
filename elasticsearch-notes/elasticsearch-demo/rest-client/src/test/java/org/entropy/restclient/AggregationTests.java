91cfad4e950279e21153f25b72329b8b4c694aba
s-chance <entropy.tree@outlook.com>
2024-09-16 20:43:53 +0800

    feat(*): using rest client to auto-complete query

--- elasticsearch-notes/elasticsearch-demo/rest-client/src/test/java/org/entropy/restclient/AggregationTests.java
+++ elasticsearch-notes/elasticsearch-demo/rest-client/src/test/java/org/entropy/restclient/AggregationTests.java
@@ -9,6 +9,10 @@ import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.search.aggregations.AggregationBuilders;
 import org.elasticsearch.search.aggregations.Aggregations;
 import org.elasticsearch.search.aggregations.bucket.terms.Terms;
+import org.elasticsearch.search.suggest.Suggest;
+import org.elasticsearch.search.suggest.SuggestBuilder;
+import org.elasticsearch.search.suggest.SuggestBuilders;
+import org.elasticsearch.search.suggest.completion.CompletionSuggestion;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -41,6 +45,25 @@ public class AggregationTests {
             long docCount = bucket.getDocCount();
             System.out.println(brandName + ": " + docCount);
         }
+    }
+
+    @Test
+    void testSuggest() throws IOException {
+        SearchRequest request = new SearchRequest("hotel");
+        request.source()
+                .suggest(new SuggestBuilder().addSuggestion(
+                        "suggestions",
+                        SuggestBuilders.completionSuggestion("suggestion")
+                                .prefix("sd")
+                                .skipDuplicates(true)
+                                .size(10)
+                ));
+        SearchResponse response = client.search(request, RequestOptions.DEFAULT);
+
+        Suggest suggest = response.getSuggest();
+        // 根据名称获取补全结果
+        CompletionSuggestion suggestions = suggest.getSuggestion("suggestions");
+        suggestions.getOptions().forEach(option -> System.out.println(option.getText().string()));
     }
 
     @BeforeEach
